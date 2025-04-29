13d40492ff34775e88d6f4d89f6afdd000fb4a76
Adam Yang <Adamyang2002@gmail.com>
2025-04-28 23:34:51 -0400

    Chore: Made text black

--- echelon/app/Components/StreakTracker.tsx
+++ echelon/app/Components/StreakTracker.tsx
@@ -1,6 +1,6 @@
 export default function StreakTracker({ days }: { days: number }) {
     return (
-      <div className="text-center my-4">
+      <div className="text-center my-4 text-black">
         🔥 Streak: <span className="font-bold">{days}</span> day{days !== 1 ? 's' : ''}
       </div>
     );
